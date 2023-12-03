beace695daa26b4f66cd20bfc47fd3fb60856492
gimenomarc <gimenocervantesmarc@gmail.com>
2023-12-03 17:50:59 +0100

    frontend ok

--- app/src/App.js
+++ app/src/App.js
@@ -1,31 +1,34 @@
-// App.js
-import React, { useState, useEffect } from 'react';
-import NewsCard from './Components/NewsCard';  // Adjust the path accordingly
-import Parser from 'rss-parser';
+// App.js con Tailwind CSS y componentes NewsCard
 
+import React from 'react';
+import NewsCard from './Components/NewsCard';
 
-const rssFeedURL = 'https://hnrss.org/newest';
+const newsItems = [
+  {
+    title: 'News Item 1 Title',
+    description: 'News Item 1 Description',
+    summary: 'News Item 1 Summary',
+  },
+  {
+    title: 'News Item 2 Title',
+    description: 'News Item 2 Description',
+    summary: 'News Item 2 Summary',
+  },
+  {
+    title: 'News Item 3 Title',
+    description: 'News Item 3 Description',
+    summary: 'News Item 3 Summary',
+  },
+];
 
-const App = () => {
-  const [newsItems, setNewsItems] = useState([]);
-
-  useEffect(() => {
-    const fetchNews = async () => {
-      const parser = new Parser();
-      const feed = await parser.parseURL(rssFeedURL);
-      setNewsItems(feed.items);
-    };
-
-    fetchNews();
-  }, []);
-
+function App() {
   return (
-    <div className="container">
-      {newsItems.map((item, index) => (
-        <NewsCard key={index} item={item} />
+    <div className="App">
+      {newsItems.map((item) => (
+        <NewsCard key={item.title} item={item} />
       ))}
     </div>
   );
-};
+}
 
 export default App;
